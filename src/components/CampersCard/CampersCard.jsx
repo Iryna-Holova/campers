9e21a83d5f2e0cce4e31f8199a46b3cb7f99178f
Iryna Holova <gis.golova@gmail.com>
2024-04-26 09:08:27 +0300

    add markup & styles

--- src/components/CampersCard/CampersCard.jsx
+++ src/components/CampersCard/CampersCard.jsx
@@ -1,7 +1,88 @@
+import ButtonLink from 'components/ButtonLink/ButtonLink';
+import icons from './CampersCard.module.css';
 import css from './CampersCard.module.css';
 
 const CampersCard = ({ camper }) => {
-  return <li>{camper.name}</li>;
+  const {
+    // _id,
+    // name,
+    // price,
+    // rating,
+    // location,
+    // adults,
+    // engine,
+    // transmission,
+    description,
+    // AC,
+    // bathroom,
+    // kitchen,
+    // beds,
+    // TV,
+    // gallery,
+    // reviews,
+    isFavorite = false,
+  } = camper;
+
+  return (
+    <li className={css.card}>
+      {/* <CamperImage images={gallery} /> */}
+      <div className={css.details}>
+        <div className={css.header}>
+          <div className={css.title}>
+            {/* <CardTitle>{name}</CardTitle> */}
+            {/* <CamperPrice price={price} /> */}
+            <button
+              type="button"
+              // onClick={() =>
+              //   isFavorite
+              //     ? dispatch(removeFavorite(_id))
+              //     : dispatch(addFavorite(camper))
+              // }
+              className={`${css.heart} ${isFavorite ? css.checked : ''}`}
+            >
+              <svg width={24} height={24}>
+                <use href={icons + '#icon-heart'} />
+              </svg>
+              <svg width={24} height={24} className={css.filled}>
+                <use href={icons + '#icon-heart-filled'} />
+              </svg>
+            </button>
+          </div>
+          {/* <CamperStats
+            rating={rating}
+            reviews={reviews.length}
+            location={location}
+            onOpenReviews={handleRatingClick}
+          /> */}
+        </div>
+        <p className={css.description}>{description}</p>
+        {/* <CamperFeatures
+          features={{
+            adults,
+            transmission,
+            engine,
+            kitchen,
+            beds,
+            AC,
+            TV,
+            'Shower/WC': bathroom,
+          }}
+        /> */}
+        <ButtonLink
+          type="button"
+          // onClick={() => setShowModal(true)}
+          size="small"
+        >
+          Show more
+        </ButtonLink>
+      </div>
+      {/* {showModal && (
+        <Modal onClose={handleModalClose}>
+          <CamperModal {...camper} modalTab={modalTab} />
+        </Modal>
+      )} */}
+    </li>
+  );
 };
 
 export default CampersCard;
--- src/components/CampersList/CampersList.jsx
+++ src/components/CampersList/CampersList.jsx
@@ -1,13 +1,35 @@
 import CampersCard from 'components/CampersCard/CampersCard';
+import ButtonLink from 'components/ButtonLink/ButtonLink';
 import css from './CampersList.module.css';
 
 const CampersList = ({ campers }) => {
   return (
-    <ul>
-      {campers.map(camper => (
-        <CampersCard key={camper._id} camper={camper} />
-      ))}
-    </ul>
+    <div className={css.wrapper}>
+      <ul className={css.list}>
+        {campers.map(camper => (
+          <CampersCard key={camper._id} camper={camper} />
+        ))}
+        {/* {isLoading && (
+          <>
+            <CamperSkeleton />
+            <CamperSkeleton />
+            <CamperSkeleton />
+            <CamperSkeleton />
+          </>
+        )} */}
+      </ul>
+      {/* {isLoadMore && ( */}
+      <ButtonLink
+        type="button"
+        // onClick={onLoadMore}
+        // disabled={isLoading}
+        size="smaller"
+        color="secondary"
+      >
+        Load more
+      </ButtonLink>
+      {/* )} */}
+    </div>
   );
 };
 
