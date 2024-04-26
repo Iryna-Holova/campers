1722d5dd814bdd259f377d91d81ea1243c161b1c
Iryna Holova <gis.golova@gmail.com>
2024-04-26 07:43:55 +0300

    shared layout

---
+++ src/components/SharedLayout.jsx
@@ -0,0 +1,22 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header/Header';
+import Loader from './Loader/Loader';
+import Footer from './Footer/Footer';
+
+const SharedLayout = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <div id="top" style={{ position: 'absolute', top: 0 }}></div>
+      <Header />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer />
+    </Suspense>
+  );
+};
+
+export default SharedLayout;
---
+++ src/constants/static.js
@@ -0,0 +1,11 @@
+export const PAGES_FOOTER = [
+  { page: 'About Us', path: '/' },
+  { page: 'Catalog', path: '/catalog' },
+  { page: 'Favorites', path: '/favorites' },
+];
+
+export const SOCIAL_LINKS = [
+  { link: 'https://www.instagram.com/', icon: '#icon-instagram' },
+  { link: 'https://www.facebook.com/', icon: '#icon-facebook' },
+  { link: 'https://www.youtube.com/', icon: '#icon-youtube' },
+];
