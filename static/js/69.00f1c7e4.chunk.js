bb4a13880ad076318e95efb5160e23aa3e537aeb
Iryna-Holova <94083644+Iryna-Holova@users.noreply.github.com>
2024-04-26 04:44:42 +0000

    Deploying to gh-pages from @ Iryna-Holova/campers@1722d5dd814bdd259f377d91d81ea1243c161b1c 🚀

---
+++ static/js/69.00f1c7e4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[69],{69:function(e,t,a){a.r(t);var r=a(329);t.default=function(){return(0,r.jsx)("div",{children:"CatalogPage"})}}}]);
+//# sourceMappingURL=69.00f1c7e4.chunk.js.map
