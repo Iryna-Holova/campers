fcf802aed85e8cf2d19203985700c9867d1505b3
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2024-05-02 19:40:48 +0300

    Update CallToAction.jsx

--- src/components/Home/CallToAction/CallToAction.jsx
+++ src/components/Home/CallToAction/CallToAction.jsx
@@ -3,6 +3,7 @@ import css from './CallToAction.module.css';
 const CallToAction = () => {
   return (
     <section className={css.section + ' container'}>
+      <h2 className='visually-hidden'>Call To Action</h2>
       <p className={css.text}>
         Ready to embark on your Ukrainian adventure? Explore our camper catalog,
         check availability, and book your dream camper today. Experience the
